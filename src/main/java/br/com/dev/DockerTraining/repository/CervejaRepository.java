1436dd46bbc796196a7aa589d1bb03124dd8b373
joaovqmartins <joavmartin3zp1n@gmail.com>
2026-06-01 21:05:40 -0300

    refactor: simplify project for Docker training

--- src/main/java/br/com/dev/DockerTraining/repository/CervejaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.dev.DockerTraining.repository;
-
-import br.com.dev.DockerTraining.model.Cerveja;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CervejaRepository extends JpaRepository<Cerveja, Long> {
-}
